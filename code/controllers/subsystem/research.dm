a810ab7a8c7af4b5b217b759c850c70c601b1b88
Uristthedorf <40842973+Uristthedorf@users.noreply.github.com>
2025-03-09 16:20:32 -0700

    first

--- code/controllers/subsystem/research.dm
+++ code/controllers/subsystem/research.dm
@@ -38,7 +38,7 @@ SUBSYSTEM_DEF(research)
 	var/list/techweb_nodes_experimental = list()
 	///path = list(point type = value)
 	var/list/techweb_point_items = list(
-	/obj/item/assembly/signaler/anomaly = list(TECHWEB_POINT_TYPE_GENERIC = 10000)
+	/obj/item/assembly/signaler/anomaly = list(TECHWEB_POINT_TYPE_GENERIC = TECHWEB_TIER_5_POINTS)
 	)
 	var/list/errored_datums = list()
 	var/list/point_types = list() //typecache style type = TRUE list
@@ -117,6 +117,13 @@ SUBSYSTEM_DEF(research)
 			techweb_list.add_point_list(bitcoins)
 
 		techweb_list.last_income = world.time
+
+		if(techweb_list.research_queue_nodes.len)
+				techweb_list.research_node_id(techweb_list.research_queue_nodes[1]) // Attempt to research the first node in queue if possible
+
+				for(var/datum/techweb_node/node as anything in techweb_list.research_queue_nodes)
+					if(node.is_free(techweb_list)) // Automatically research all free nodes in queue if any
+						techweb_list.research_node(node)
 
 	for(var/core_type in slime_core_prices)
 		var/obj/item/slime_extract/core = core_type
--- tgui/packages/tgui/interfaces/Techweb.jsx
+++ tgui/packages/tgui/interfaces/Techweb.jsx
@@ -152,6 +152,8 @@ export const TechwebContent = (props) => {
     t_disk,
     d_disk,
     locked,
+	queue_nodes = [],
+    node_cache,
   } = data;
   const [techwebRoute, setTechwebRoute] = useLocalState('techwebRoute', null);
   const [lastPoints, setLastPoints] = useLocalState('lastPoints', {});
@@ -161,27 +163,35 @@ export const TechwebContent = (props) => {
       <Flex.Item className="Techweb__HeaderSection">
         <Flex className="Techweb__HeaderContent">
           <Flex.Item>
-            <Box>
-              Available points:
-              <ul className="Techweb__PointSummary">
-                {Object.keys(points).map((k) => (
-                  <li key={k}>
-                    <b>{k}</b>: {points[k]}
-                    {!!points_last_tick[k] && ` (+${points_last_tick[k]}/sec)`}
-                  </li>
-                ))}
-              </ul>
-            </Box>
-            <Box>
-              Security protocols:
-              <span
-                className={`Techweb__SecProtocol ${
-                  !!sec_protocols && 'engaged'
-                }`}
-              >
-                {sec_protocols ? 'Engaged' : 'Disengaged'}
-              </span>
-            </Box>
+            <LabeledList>
+              <LabeledList.Item label="Security">
+                <span
+                  className={`Techweb__SecProtocol ${
+                    !!sec_protocols && 'engaged'
+                  }`}
+                >
+                  {sec_protocols ? 'Engaged' : 'Disengaged'}
+                </span>
+              </LabeledList.Item>
+              {Object.keys(points).map((k) => (
+                <LabeledList.Item key={k} label="Points">
+                  <b>{points[k]}</b>
+                  {!!points_last_tick[k] && ` (+${points_last_tick[k]}/sec)`}
+                </LabeledList.Item>
+              ))}
+              <LabeledList.Item label="Queue">
+                {queue_nodes.length !== 0
+                  ? Object.keys(queue_nodes).map((node_id) => (
+                      <Button
+                        key={node_id}
+                        tooltip={`Added by: ${queue_nodes[node_id]}`}
+                      >
+                        {node_cache[node_id].name}
+                      </Button>
+                    ))
+                  : 'Empty'}
+              </LabeledList.Item>
+            </LabeledList>
           </Flex.Item>
           <Flex.Item grow={1} />
           <Flex.Item>
@@ -488,9 +498,16 @@ const TechNodeDetail = (props) => {
 
 const TechNode = (props) => {
   const { act, data } = useRemappedBackend();
-  const { node_cache, design_cache, experiments, points, nodes } = data;
+  const { node_cache, design_cache, experiments, points, nodes, queue_nodes = [],} = data;
   const { node, nodetails, nocontrols } = props;
-  const { id, can_unlock, tier } = node;
+  const {
+    id,
+    can_unlock,
+    have_experiments_done,
+    tier,
+    enqueued_by_user,
+    is_free,
+  } = node;
   const {
     name,
     description,
@@ -550,6 +567,40 @@ const TechNode = (props) => {
       buttons={
         !nocontrols && (
           <>
+		  {tier > 0 &&
+              (!!can_unlock && (is_free || queue_nodes.length === 0) ? (
+                <Button
+                  icon="lightbulb"
+                  disabled={!can_unlock || tier > 1 || queue_nodes.length > 0}
+                  onClick={() => act('researchNode', { node_id: id })}
+                >
+                  Research
+                </Button>
+              ) : enqueued_by_user ? (
+                <Button
+                  icon="trash"
+                  color="bad"
+                  onClick={() => act('dequeueNode', { node_id: id })}
+                >
+                  Dequeue
+                </Button>
+              ) : id in queue_nodes && !enqueued_by_user ? (
+                <Button icon="check" color="good">
+                  Queued
+                </Button>
+              ) : (
+                <Button
+                  icon="lightbulb"
+                  disabled={
+                    !have_experiments_done ||
+                    id in queue_nodes ||
+                    techcompl < prereq_ids.length
+                  }
+                  onClick={() => act('enqueueNode', { node_id: id })}
+                >
+                  Enqueue
+                </Button>
+              ))}
             {!nodetails && (
               <Button
                 icon="tasks"
@@ -559,15 +610,6 @@ const TechNode = (props) => {
                 }}
               >
                 Details
-              </Button>
-            )}
-            {tier > 0 && (
-              <Button
-                icon="lightbulb"
-                disabled={!can_unlock || tier > 1}
-                onClick={() => act('researchNode', { node_id: id })}
-              >
-                Research
               </Button>
             )}
           </>
